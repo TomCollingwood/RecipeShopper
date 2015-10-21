3f078635a096e7ce770880c45247b8aa794a8ec2
Tom Collingwood <tomcoll@hotmail.co.uk>
2015-10-21 17:41:46 +0100

    added edit and worked on add

--- RecipeShopper.py
+++ RecipeShopper.py
@@ -15,6 +15,34 @@ class recipe:
 		else:
 			raise ValueError
 
+def updaterecipetitle(index, newtitle):
+	recipes[index] = recipe(newtitle, recipes[index].recipe, recipes[index].ingredients)
+
+def updatereciperecipe(index,newrecipe):
+	recipes[index] = recipe(recipes[index].title, newrecipe, recipes[index].ingredients)
+
+def updaterecipeingredientname(recipesindex,ingrindex,newname):
+	try:
+		recipes[recipesindex].ingredients[ingrindex] = { ingredient(newname, 
+			recipes[recipesindex].ingredients[ingrindex].amount,
+			recipes[recipesindex].ingredients[ingrindex].gram) }
+	except IndexError:
+		print "That ingredient does not exist"
+
+def updaterecipeingredientamount(ingredienttochange,newamount):
+	if newamount.endswith("g"):
+		ingredienttochange.gram = True
+		ingredienttochange.amount = newamount[:-1]
+	elif newamount.endswith("kg"):
+		ingredienttochange.gram = True
+		ingredienttochange.amount = newamount[:-1] * 1000
+	elif newamount.endswith("mg"):
+		ingredienttochange.gram = True
+		ingredienttochange.amount = newamount[:-1] / 1000
+	else:
+		ingredienttochange.ram = False
+		ingredienttochange.amount = newamount
+
 def exportrecipe(filename, recipe1):
 	# We can safely assume recipe1 is a recipe. As write recipe is only called by writerecipes.
 	# And user can only edit recipes list with defined commands at terminal
@@ -45,8 +73,7 @@ def importrecipes(filename):
 	
 	num_lines = sum(1 for line in open(filename))
 	
-	content = fh.readlines()
-	lineedit = ""	
+	content = fh.readlines()	
 	linelist = []
 	linenumber = 0 
 	for line in content:
@@ -65,18 +92,21 @@ def importrecipes(filename):
 	fh.close()
 	return
 
+def printrecipe(index):
+	print "Name: "+recipe[index].name+"\n"
+	print "Recipe: "+recipe[index].recipe+"\n"
+	print "Ingredients: \n"
+	for ingredient in recipe[index].ingredients:
+		if(ingredient.gram):
+			print ingredient.amount+"g of "+ingredient.name+"\n"
+		else:
+			if (ingredient.amount>1):
+				print ingredient.amount+ingredient.name+"s \n"
+	print "\n"
+
 def printrecipes():
-	for item in recipes:
-		print "Name: "+item.name+"\n"
-		print "Recipe: "+item.recipe+"\n"
-		print "Ingredients: \n"
-		for ingredient in item.ingredients:
-			if(ingredient.gram):
-				print ingredient.amount+"g of "+ingredient.name+"\n"
-			else:
-				if (ingredient.amount>1):
-					print ingredient.amount+ingredient.name+"s \n"
-		print "\n"
+	for index in range(len(recipes)):
+		printrecipe(index);
 		
 quit = False
 correct = False
@@ -113,37 +143,53 @@ while not quit:
 			while True:
 
 				input_ingr_amount = raw_input("What is the amount of "+input_ingr_name+" ?\n")
-
-				if input_ingr_amount.endswith("g"):
-					input_ingr_gram = True
-					input_ingr_amount_fixed = input_ingr_amount[:-1]
-				elif input_ingr_amount.endswith("kg"):
-					input_ingr_gram = True
-					input_ingr_amount_fixed = input_ingr_amount[:-1] * 1000
-				elif input_ingr_amount.endswith("mg"):
-					input_ingr_gram = True
-					input_ingr_amount_fixed = input_ingr_amount[:-1] / 1000
-				else:
-					input_ingr_gram = False
-					input_ingr_amount_fixed = input_ingr_amount
-
+				newingredient = ingredient(input_ingr_name, 0,False)
+				
 				confirm = raw_input(input_ingr_amount+"\nIs this amount correct? (y/n)")
 				while(confirm != "y" and confirm != "n"):
 					confirm = raw_input(input_ingr_amount+"\nIs this amount correct?  (y/n) \n")
+
 				if (confirm == "y"):
+					updaterecipeingredientamount(newingredient,input_ingr_amount)
 					break
 
-			input_ingredients.append(ingredient(input_ingr_name, input_ingr_amount_fixed,input_ingr_gram))
+			input_ingredients.append(newingredient)
 			confirm = raw_input("Add more ingredients? (y/n) \n")
-			while(confirm != y or confirm != n):
+			while(confirm != y and confirm != n):
 				confirm = raw_input("Add more ingredients? (y/n) \n")
 			if (confirm == "n"):
 				break
-			else:
-				return
 
+				
 		recipes.append(recipe(input_title,input_recipe,input_ingredients))
 		print "Recipe Added!"
+	if (user_input == "edit"):
+		counter = 1
+		for r in recipes:
+			print "("+counter+") "+r.title+"\n"
+		index = raw_input("Enter the integer of the one you want to edit: \n")
+		while type(index) != int:
+			index = raw_input("I need the INTEGER of the one you want to edit: \n")
+		print "Here is your recipe:\n\n"
+		printrecipe(index-1)
+		response =  raw_input("What would you like to edit? (name, recipe or ingredient 1, ingredient 2 ...)")
+		if(response=="name"):
+			newinfo = raw_input("What would you like to change it to?")
+			updaterecipetitle(newinfo)
+		elif(response=="recipe"):
+			newinfo = raw_input("What would you like to change it to?")
+			updatereciperecipe(newinfo)
+		elif(response.startswith("ingredient")):
+			responsesplit = response.split(" ")
+			whichone = raw_input("Would you like to edit the name or amount of this ingredient?")
+			newinfo = raw_input("What would you like to change it to?")
+			if(whichone == "name"):
+				updaterecipeingredientname(index-1,int(responsesplit[1]),newinfo)
+			elif(whichone == "amount"):
+				updaterecipeingredientamount(index-1,int(responsesplit[1]),newinfo)
+			else:
+				print "You screwed up mate. You have to start again because I'm a lazy coder."
+
 	if (user_input == "quit"):
 		quit = True
 
